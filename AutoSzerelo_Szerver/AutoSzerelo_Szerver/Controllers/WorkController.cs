49a17e0cd717c57c745025bed25450f3bc8dd84d
Rácz Marco <raczmarco3@gmail.com>
2020-04-19 10:01:27 +0200

    Work és WorkController létrehozása.

---
+++ AutoSzerelo_Szerver/AutoSzerelo_Szerver/Controllers/WorkController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoSzerelo_Szerver.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoSzerelo_Szerver.Controllers
+{
+    [Route("api/work")]
+    [ApiController]
+    public class WorkController : ControllerBase
+    {
+        private Work[] works = new Work[]
+        {
+            new Work { WorkId=1, ClientName="Lakatos József", CarType="Ford", LicensePlate="ASD-123", Problem="valami baj van"},
+            new Work { WorkId=2, ClientName="Tóth János", CarType="Nissan", LicensePlate="BTR-142", Problem="asdlol"}
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Work>> Get()
+        {
+            return Ok(works);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Work> Get(long id)
+        {
+            var work = works.FirstOrDefault(i => i.WorkId == id);
+
+            if(work != null)
+            {
+                return work;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ AutoSzerelo_Szerver/AutoSzerelo_Szerver/Models/Work.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoSzerelo_Szerver.Models
+{
+    public class Work
+    {
+        public long WorkId { get; set; }
+        public string ClientName { get; set; }
+        public string CarType { get; set; }
+        public string LicensePlate { get; set; }
+        public string Problem { get; set; }
+
+    }
+}
